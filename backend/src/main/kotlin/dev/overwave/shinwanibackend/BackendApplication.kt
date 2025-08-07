68457ecf38a7c2e5f4a860fb93c0e727495caeff
Rodion Kabanov <overwave2@gmail.com>
2025-08-07 19:56:57 +0300

    backend now is runnable

--- backend/src/main/kotlin/dev/overwave/shinwanibackend/BackendApplication.kt
+++ backend/src/main/kotlin/dev/overwave/shinwanibackend/BackendApplication.kt
@@ -1,9 +1,10 @@
 package dev.overwave.shinwanibackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 class BackendApplication
 
 fun main(args: Array<String>) {
