03fd6813c41e551a67580e48242ee024e35ce48d
Rodion Kabanov <overwave2@gmail.com>
2025-08-07 19:46:14 +0300

    Initial commit

---
+++ backend/src/test/kotlin/dev/overwave/shinwanibackend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.overwave.shinwanibackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
