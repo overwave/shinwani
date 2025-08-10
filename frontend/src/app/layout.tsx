55604dee30077690f7f0f9cc7746a502125d5111
Rodion Kabanov <overwave2@gmail.com>
2025-08-11 01:18:36 +0300

    added login background

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,22 +1,23 @@
 import "./globals.scss";
-import { ReactNode } from "react";
+import {ReactNode} from "react";
+import {Metadata} from "next";
 
-export const metadata = {
-  icons: {
-    icon: [
-      { url: '/icon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/icon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-    ],
-    apple: [
-      { url: '/icon/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
-    ],
-    other: [
-      { rel: 'manifest', url: '/icon/site.webmanifest' },
-    ],
-  },
+export const metadata: Metadata = {
+    icons: {
+        icon: [
+            {url: '/icon/favicon-16x16.png', sizes: '16x16', type: 'image/png'},
+            {url: '/icon/favicon-32x32.png', sizes: '32x32', type: 'image/png'},
+        ],
+        apple: [
+            {url: '/icon/apple-touch-icon.png', sizes: '180x180', type: 'image/png'},
+        ],
+        other: [
+            {rel: 'manifest', url: '/icon/site.webmanifest'},
+        ],
+    },
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({children}: { children: ReactNode }) {
     return (
         <html lang="en">
         <body>{children}</body>
