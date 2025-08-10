5578327f688972fed62345bb0a42f88a3967580f
Rodion Kabanov <overwave2@gmail.com>
2025-08-10 23:42:42 +0300

    added favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,21 @@
 import "./globals.scss";
 import { ReactNode } from "react";
 
+export const metadata = {
+  icons: {
+    icon: [
+      { url: '/icon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/icon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/icon/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+    other: [
+      { rel: 'manifest', url: '/icon/site.webmanifest' },
+    ],
+  },
+};
+
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang="en">
