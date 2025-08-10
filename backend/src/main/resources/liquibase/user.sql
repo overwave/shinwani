74c764dece298a2c99c8f8e3285aa0042a888acb
Rodion Kabanov <overwave2@gmail.com>
2025-08-11 01:52:15 +0300

    added liquibase for backend

---
+++ backend/src/main/resources/liquibase/user.sql
@@ -0,0 +1,13 @@
+--liquibase formatted sql
+
+--changeset overwave:user_create_table
+CREATE TABLE IF NOT EXISTS user_
+(
+    id           BIGSERIAL PRIMARY KEY,
+    login        TEXT        NOT NULL,
+    wanikani_key TEXT        NOT NULL,
+    bunpro_key   TEXT        NOT NULL,
+    password     TEXT        NOT NULL,
+    created_at   TIMESTAMPTZ NOT NULL,
+    updated_at   TIMESTAMPTZ NOT NULL
+);
